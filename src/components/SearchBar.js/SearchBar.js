911132b6b7c8246d37e7c2c1d27fd876c5c019ad
Sameer Utradhi <geekysam7@gmail.com>
2020-05-03 03:30:13 +0530

    starter

--- src/components/SearchBar.js/SearchBar.js
+++ src/components/SearchBar.js/SearchBar.js
@@ -10,6 +10,7 @@ function SearchBar() {
     const [buttonClicked, setButtonClicked] = useState('');
     const [queryData, setQueryData] = useState([]);
     // const [limit, setLimit] = useState(25);
+    const inputRef = React.createRef();
 
     const callApi = () => {
         setButtonClicked(search);
@@ -32,7 +33,10 @@ function SearchBar() {
                     e.preventDefault();
                 }}>
                     <div className={styles.inputContainer}>
-                    <input type="text" value={search} onChange={e => setSearchState(e.target.value)} className={styles.inputBox} autoFocus placeholder="Search"/>
+                    <input type="text" value={search} onChange={e => setSearchState(e.target.value)} className={styles.inputBox} autoFocus placeholder="Search"
+                    ref={inputRef}
+                    onFocus={e => e.target.select()}
+                    />
                     </div>
                     <button onClick={callApi} className={styles.searchButton}>
                         <img src="https://images.vexels.com/media/users/3/147105/isolated/preview/5c38be8d63c0ae0ad034417daaef3bcb-instagram-search-icon-by-vexels.png" alt="search" className={styles.searchImage}></img>
